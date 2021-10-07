fb464339b622aa4ea4d828255a2c9a080e4505c7
kodai-kato <greatvoyage0122@gmail.com>
2021-10-07 22:14:37 +0900

    Play

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,7 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require("@rails/webpacker");
+const erb = require("./loaders/erb");
+const provide = require("./plugins/provide");
 
-module.exports = environment
+environment.loaders.prepend("erb", erb);
+environment.loaders.prepend("provide", provide);
+module.exports = environment;
